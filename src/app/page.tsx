157ef02defa0bb00ade5193f33bf91e0e30dee2f
Polina Limonova <polina98_08@mail.ru>
2024-03-25 15:49:25 +0300

    NextAuth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
 import Dashboard from "./dashboard/page";
 import Header from "@/components/Header/Header";
 import SideMenu from "@/components/SideMenu/SideMenu";
+import Login from "@/components/Login/Login";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 
-export default function Home() {
+interface Props {
+  session: Session | null;
+}
+
+const Home: React.FC<Props> = ({ session }) => {
   return (
     <main>
-      <Header />
-      <SideMenu />
-      <Dashboard />
+      <SessionProvider session={session}>
+        <Header />
+        <SideMenu />
+        <Dashboard />
+        <Login />
+      </SessionProvider>
     </main>
   );
-}
+};
+
+export default Home;
