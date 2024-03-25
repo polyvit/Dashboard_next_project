cfe483de121a7dc3a82bda0b9fb42403724ef3a0
Polina Limonova <polina98_08@mail.ru>
2024-03-25 13:09:21 +0300

    Routing preparation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Dashboard from "./dashboard/page";
+import Header from "@/components/Header/Header";
+import SideMenu from "@/components/SideMenu/SideMenu";
 
 export default function Home() {
-  return <main>{/* <Dashboard /> */}</main>;
+  return (
+    <main>
+      <Header />
+      <SideMenu />
+      <Dashboard />
+    </main>
+  );
 }
