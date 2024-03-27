d9975a1aaa7a123a27f8a80ba93b39197de99739
Polina Limonova <polina98_08@mail.ru>
2024-03-27 10:59:37 +0300

    Add Drawer MaterialUI to SideMenu component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,8 +17,6 @@ import AdbIcon from "@mui/icons-material/Adb";
 import ThemeToggleBtn from "../ThemeToggle/ThemeToggleBtn";
 import { useMediaQuery } from "@mui/material";
 
-const pages = ["Products", "Pricing", "Blog"];
-
 export default function Header({ ColorModeContext }) {
   const { data: session } = useSession();
   const tabletCheck = useMediaQuery("(min-width: 768px)");
@@ -69,43 +67,6 @@ export default function Header({ ColorModeContext }) {
           >
             DASHBOARD
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
           <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Typography
             variant="h5"
@@ -125,19 +86,8 @@ export default function Header({ ColorModeContext }) {
           >
             LOGO
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
           {tabletCheck && (
-            <Box sx={{ paddingRight: 5 }}>
+            <Box sx={{ paddingRight: 5, marginLeft: "auto" }}>
               <Typography>Signed in as {session?.user?.email}</Typography>
             </Box>
           )}
--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,11 +1,128 @@
-import React from "react";
+import * as React from "react";
+import { useState } from "react";
+import Link from "next/link";
+import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import EqualizerIcon from "@mui/icons-material/Equalizer";
+import Person2Icon from "@mui/icons-material/Person2";
+import SettingsIcon from "@mui/icons-material/Settings";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Drawer from "@mui/material/Drawer";
+
+import styles from "./SideMenu.module.scss";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const menuRouteList = ["analytics", "profile", "settings", ""];
+const menuTitleList = ["Analytics", "Profile", "Settings", "Sign Out"];
+const menuIconsList = [
+  <EqualizerIcon />,
+  <Person2Icon />,
+  <SettingsIcon />,
+  <ExitToAppIcon />,
+];
 
 export default function SideMenu() {
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const mobileCheck = useMediaQuery("(min-width: 600px)");
+
+  const handleDrawerToggle = () => {
+    setOpen(!open);
+  };
+
   return (
-    <ul>
-      <li>Analytics</li>
-      <li>Profile</li>
-      <li>Settings</li>
-    </ul>
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      open={open}
+      sx={{
+        width: drawerWidth,
+        ["& .MuiDrawer-paper"]: {
+          top: mobileCheck ? 69 : 57,
+          flexShrink: 0,
+          whiteSpace: "nowrap",
+          boxSizing: "border-box",
+          ...(open && {
+            ...openedMixin(theme),
+            "& .MuiDrawer-paper": openedMixin(theme),
+          }),
+          ...(!open && {
+            ...closedMixin(theme),
+            "& .MuiDrawer-paper": closedMixin(theme),
+          }),
+        },
+      }}
+    >
+      <div className={styles.drawHeader}>
+        <IconButton onClick={handleDrawerToggle}>
+          {theme.direction === "rtl" ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+      </div>
+      <Divider />
+      <List>
+        {menuTitleList.map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: "block" }}>
+            <Link
+              className={styles.link}
+              href={`/dashboard/${menuRouteList[index]}`}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {menuIconsList[index]}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
 }
